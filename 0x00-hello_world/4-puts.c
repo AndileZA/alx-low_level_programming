b3207c3b7b3e56fd296bb19ee13f2d69dc6f428f
AndileZA <nangcobo93@gmail.com>
2022-09-11 00:26:25 -0700

    cll

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+*Print string using puts function
+*
+* this program prints the string - "Programming is like building a multilingual puzzle
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
