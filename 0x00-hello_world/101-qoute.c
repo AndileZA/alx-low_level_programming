186039460ebbe681bec0c2f4b60e40e86c22dc59
AndileZA <nangcobo93@gmail.com>
2022-09-11 09:53:48 -0700

    cll

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print using printf function
+ *
+ * Description: using main function
+ * this program prints string
+ * Return: 1
+ */
+int main(void)
+{
+	printf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
+
