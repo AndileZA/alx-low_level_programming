cbf24159ed0cc1a5b9eb871f67d734ae38ae8195
AndileZA <nangcobo93@gmail.com>
2022-09-12 16:59:39 -0700

    car

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the possible combinations of sigle digit numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		putchar((num % 10) + '0');
+		if (num == 9)
+			countinue;
+
+		putchar(',');
+		putchar(' ');
+	
+	putchar('\n');
+
+	return (0);
+}
