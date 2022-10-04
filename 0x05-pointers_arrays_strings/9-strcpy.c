66199f82c7cea91be9669d42e94c5212f6aba4b1
AndileZA <nangcobo93@gmail.com>
2022-10-04 11:05:19 +0200

    0x05

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,14 @@
  */
 char *_strcpy(char *dest, const char *src)
 {
-	int index = 0;
+        int index = 0;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
+        while (src[index])
+        {
+                dest[index] = src[index];
+                index++;
+        }
 
-	return (dest);
+        return (dest);
 }
+
