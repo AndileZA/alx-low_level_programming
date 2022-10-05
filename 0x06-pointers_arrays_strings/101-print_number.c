03727e0b85a882225a63787c23284ffb571555c8
AndileZA <nangcobo93@gmail.com>
2022-10-05 09:37:30 +0200

    0x06

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_number(int n)
+{
+    // If number is smaller than 0, put a - sign
+    // and change number to positive
+    if (n < 0) {
+        putchar('-');
+        n = -n;
+    }
+
+    // Remove the last digit and recur
+    if (n/10)
+        print_number(n/10);
+
+    // Print the last digit
+    putchar(n%10 + '0');
+}
